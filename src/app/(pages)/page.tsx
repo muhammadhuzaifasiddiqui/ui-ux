e80c89d9ae17012ab8b24cf4641def01be086dac
Huzaifa <huzaifasiddiquiep@gmail.com>
2025-09-04 09:08:45 +0500

    add

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -10,28 +10,39 @@ const page = () => {
       <section className="hero-section">
         <div className="background-color pt-50 pb-40">
           <div className="w-[25%]">
-            <p></p>
-            <h1></h1>
-            <Link href="#" className="btn"></Link>
-            <p></p>
-            <div className="flex">
+            <p className="text-gray font-normal">
+              Hi.... <br /> My name is <span className="font-bold">Taha Khanzada</span>
+            </p>
+            <h1 className="font-bold text-gray">UI/UX</h1>
+            <Link href="#" className="btn btn-gray-right btn-md">
+              {" "}
+              Design
+            </Link>
+            <p>
+              Simplifying complex ideas into clear and engaging interfaces,
+              ensuring that every interaction is intuitive, efficient, and
+              visually pleasing.
+            </p>
+            <div className="flex items-center">
               <div className="w-[45%]">
-                <p>
-                  <span></span>
+                <p>35$
+                  <span>/hr</span>
                 </p>
               </div>
-              <div className="w-[35%]">
-                <Link href="#" className="btn"></Link>
-                <Link href="#" className="btn"></Link>
-                <Link href="#" className="btn"></Link>
+              <div className="w-[35%] flex flex-col">
+                <Link href="#" className="btn">Wireframes</Link>
+                <Link href="#" className="btn">Insights</Link>
+                <Link href="#" className="btn">Testimonials</Link>
               </div>
             </div>
             <div className="flex gap-4 mt-20">
-              <Link href="#" className="btn"></Link>
-              <Link href="#" className="btn"></Link>
+              <Link href="#" className="btn">Get in touch</Link>
+              <Link href="#" className="btn">Show my work</Link>
             </div>
           </div>
-          <div className="w-[65%]"></div>
+          <div className="w-[65%]">
+
+          </div>
         </div>
       </section>
     </>
